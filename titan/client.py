92837b7651b8966ea49e20fc879381cf396a1649
TJ Murphy <1796+teej@users.noreply.github.com>
2023-08-11 17:22:29 -0700

    simpify connection for now

--- titan/client.py
+++ titan/client.py
@@ -2,20 +2,11 @@ import os
 import snowflake.connector
 
 connection_params = {
-    "account": os.environ["SNOWFLAKE_ACCOUNT"],
-    "user": os.environ["SNOWFLAKE_USER"],
-    "password": os.environ["SNOWFLAKE_PASSWORD"],
+    "account": os.environ.get("SNOWFLAKE_ACCOUNT"),
+    "user": os.environ.get("SNOWFLAKE_USER"),
+    "password": os.environ.get("SNOWFLAKE_PASSWORD"),
 }
 
 
 def get_session():
     return snowflake.connector.connect(**connection_params)
-
-
-# from snowflake.snowpark import Session
-
-# connection_params = {}
-
-
-# def get_session(account):
-#     return Session.builder.configs(account=account, **connection_params).create()
