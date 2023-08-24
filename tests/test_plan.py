f8229b2298ade7c1baeb526b34d9e517bb3915c1
TJ Murphy <1796+teej@users.noreply.github.com>
2023-08-24 11:42:03 -0700

    resource coverage for v0.1

--- tests/test_plan.py
+++ tests/test_plan.py
@@ -1,41 +1,159 @@
-import os
 import unittest
 
-from random import randint
-
-from titan import Blueprint, User, Role, RoleGrant, DataProvider
-from titan.client import get_session
-from titan.identifiers import URN
+from titan.blueprint import _plan
 
 
 class TestPlan(unittest.TestCase):
     def setUp(self):
-        self.seed = randint(100000, 1000000)
-        self.user = User(name=f"TEST_USER_{self.seed}")
-        self.session = get_session()
-        self.user_urn = URN.from_resource(account=self.session.account, resource=self.user)
-        provider = DataProvider(self.session)
-        provider.create_resource(self.user_urn, self.user.model_dump(exclude_none=True))
+        self.remote_state = {
+            "urn:XYZ123:database/RAW": {
+                "name": "RAW",
+                "transient": False,
+                "owner": "SYSADMIN",
+                "data_retention_time_in_days": 1,
+                "max_data_extension_time_in_days": 14,
+            },
+            "urn:XYZ123:database/ANALYTICS": {
+                "name": "ANALYTICS",
+                "transient": False,
+                "owner": "SYSADMIN",
+                "data_retention_time_in_days": 1,
+                "max_data_extension_time_in_days": 14,
+            },
+            "urn:XYZ123:warehouse/LOADING": {
+                "name": "LOADING",
+                "owner": "SYSADMIN",
+                "warehouse_type": "STANDARD",
+                "auto_suspend": 600,
+                "auto_resume": True,
+                "max_concurrency_level": 8,
+                "statement_queued_timeout_in_seconds": 0,
+                "statement_timeout_in_seconds": 172800,
+            },
+            "urn:XYZ123:warehouse/TRANSFORMING": {
+                "name": "TRANSFORMING",
+                "owner": "SYSADMIN",
+                "warehouse_type": "STANDARD",
+                "warehouse_size": "LARGE",
+                "auto_suspend": 600,
+                "auto_resume": True,
+                "max_concurrency_level": 8,
+                "statement_queued_timeout_in_seconds": 0,
+                "statement_timeout_in_seconds": 172800,
+            },
+            "urn:XYZ123:warehouse/REPORTING": {
+                "name": "REPORTING",
+                "owner": "SYSADMIN",
+                "warehouse_type": "STANDARD",
+                "warehouse_size": "SMALL",
+                "auto_suspend": 60,
+                "auto_resume": True,
+                "max_concurrency_level": 8,
+                "statement_queued_timeout_in_seconds": 0,
+                "statement_timeout_in_seconds": 300,
+            },
+            "urn:XYZ123:role/LOADER": {"name": "LOADER", "owner": "SYSADMIN"},
+            "urn:XYZ123:role/TRANSFORMER": {"name": "TRANSFORMER", "owner": "SYSADMIN"},
+            "urn:XYZ123:role/REPORTER": {"name": "REPORTER", "owner": "SYSADMIN"},
+            "urn:XYZ123:user/TEEJ": {
+                "name": "TEEJ",
+                "owner": "USERADMIN",
+                "must_change_password": False,
+                "disabled": False,
+                "default_role": "REPORTER",
+            },
+            "urn:XYZ123:role_grant/REPORTER": {
+                "to_role": [{"role": "REPORTER", "to_role": "SYSADMIN", "owner": "SYSADMIN"}],
+                "to_user": [{"role": "REPORTER", "to_user": "TEEJ", "owner": "SYSADMIN"}],
+            },
+            "urn:XYZ123:role_grant/LOADER": {
+                "to_role": [{"role": "LOADER", "to_role": "SYSADMIN", "owner": "SYSADMIN"}],
+                "to_user": [],
+            },
+            "urn:XYZ123:role_grant/TRANSFORMER": {
+                "to_role": [{"role": "TRANSFORMER", "to_role": "SYSADMIN", "owner": "SYSADMIN"}],
+                "to_user": [],
+            },
+        }
 
-    def test_role_permissions(self):
-        role = Role(name=f"TEST_ROLE_{self.seed}")
-        # GRANTS
-        user_grant = RoleGrant(role=role, to_user=self.user)
-        sysadmin_grant = RoleGrant(role=role, to_role="SYSADMIN")
+        self.manifest = {
+            "urn:XYZ123:database/RAW": {
+                "name": "RAW",
+                "data_retention_time_in_days": 1,
+                "transient": False,
+                "owner": "SYSADMIN",
+                "max_data_extension_time_in_days": 14,
+            },
+            "urn:XYZ123:warehouse/LOADING": {
+                "name": "LOADING",
+                "owner": "SYSADMIN",
+                "warehouse_type": "STANDARD",
+                "warehouse_size": "X-SMALL",
+                "auto_suspend": 600,
+                "auto_resume": True,
+                "max_concurrency_level": 8,
+                "statement_queued_timeout_in_seconds": 0,
+                "statement_timeout_in_seconds": 172800,
+            },
+            "urn:XYZ123:warehouse/TRANSFORMING": {
+                "name": "TRANSFORMING",
+                "owner": "SYSADMIN",
+                "warehouse_type": "STANDARD",
+                "warehouse_size": "LARGE",
+                "auto_suspend": 600,
+                "auto_resume": True,
+                "max_concurrency_level": 8,
+                "statement_queued_timeout_in_seconds": 0,
+                "statement_timeout_in_seconds": 99999,
+            },
+            "urn:XYZ123:warehouse/REPORTING": {
+                "name": "REPORTING",
+                "owner": "SYSADMIN",
+                "warehouse_type": "STANDARD",
+                "warehouse_size": "SMALL",
+                "auto_suspend": 60,
+                "auto_resume": True,
+                "max_concurrency_level": 8,
+                "statement_queued_timeout_in_seconds": 0,
+                "statement_timeout_in_seconds": 172800,
+            },
+            "urn:XYZ123:role/TRANSFORMER": {"name": "TRANSFORMER", "owner": "SYSADMIN"},
+            "urn:XYZ123:user/TEEJ": {
+                "name": "TEEJ",
+                "login_name": "TEEJ",
+                "display_name": "TEEJ",
+                "disabled": False,
+                "must_change_password": False,
+                "default_role": "REPORTER",
+                "ext_authn_duo": False,
+                "owner": "USERADMIN",
+                "has_password": False,
+                "has_rsa_public_key": False,
+            },
+            "urn:XYZ123:role_grant/TRANSFORMER": {
+                "to_role": [{"role": "TRANSFORMER", "owner": "SYSADMIN", "to_role": "SYSADMIN"}],
+                "to_user": [],
+            },
+        }
 
-        bp = Blueprint(name=f"test-{self.seed}", account=os.environ["SNOWFLAKE_ACCOUNT"])
-        bp.add(
-            role,
-            user_grant,
-            sysadmin_grant,
+    def test_plan_add_action(self):
+        plan = _plan(self.remote_state, self.manifest)
+        self.assertIn(("create", "urn:XYZ123:database/ANALYTICS", {...}), plan.changes)
+
+    def test_plan_change_action(self):
+        plan = _plan(self.remote_state, self.manifest)
+        self.assertIn(
+            ("update", "urn:XYZ123:warehouse/REPORTING", {"statement_timeout_in_seconds": 300}),
+            plan.changes,
         )
-        plan = bp.plan(self.session)
-        self.assertEqual(len(plan.changes), 2)
-        bp.apply(self.session, plan)
-        drift = bp.plan(self.session)
-        self.assertEqual(len(drift.changes), 0)
-        bp.destroy(self.session)
 
-    def tearDown(self):
-        provider = DataProvider(self.session)
-        provider.drop_resource(self.user_urn, self.user.model_dump(exclude_none=True))
+    def test_plan_remove_action(self):
+        plan = _plan(self.remote_state, self.manifest)
+        self.assertIn(("delete", "urn:XYZ123:role_grant/TRANSFORMER", {...}), plan.changes)
+
+    def test_plan_unexpected_action(self):
+        with self.assertRaises(Exception) as context:
+            # Here, you'd need to modify either remote_state or manifest or the diff function to return an unexpected action
+            plan = _plan(self.remote_state, self.manifest)
+
+        self.assertEqual(str(context.exception), "Unexpected action some_unexpected_action in diff")
--- tools/check_resource_coverage.py
+++ tools/check_resource_coverage.py
@@ -1,25 +1,44 @@
 import titan
 
-from tabulate import tabulate
+from tabulate import tabulate, SEPARATING_LINE
+
+CRITICAL = [
+    "database",
+    "schema",
+    "table",
+    "view",
+    "user",
+    "role",
+    "warehouse",
+]
 
 
 def check_resource_coverage():
-    table = []
+    critical = []
+    all_others = []
     classes = sorted(titan.Resource.classes.items())
     for resource_key, resource_cls in classes:
         if resource_cls == titan.Resource:
             continue
         from_sql = hasattr(resource_cls, "from_sql")
         create_sql = hasattr(resource_cls, "create_sql")
+        lifecycle = resource_cls.lifecycle is not None
         fetch = hasattr(titan.DataProvider, f"fetch_{resource_key}")
         create = hasattr(titan.DataProvider, f"create_{resource_key}")
         update = hasattr(titan.DataProvider, f"update_{resource_key}")
         drop = hasattr(titan.DataProvider, f"drop_{resource_key}")
-        table.append(
+
+        if resource_key in CRITICAL:
+            add_to = critical
+        else:
+            add_to = all_others
+
+        add_to.append(
             [
                 resource_cls.__name__,
                 "✔" if from_sql else "-",
                 "✔" if create_sql else "-",
+                "✔" if lifecycle else "-",
                 "✔" if fetch else "-",
                 "✔" if create else "-",
                 "✔" if update else "-",
@@ -27,7 +46,18 @@ def check_resource_coverage():
             ]
         )
 
-    print(tabulate(table, headers=["Resource Name", "from SQL", "to SQL", "fetch", "create", "update", "drop"]))
+    headers = [
+        "Resource Name",
+        "from SQL",
+        "to SQL",
+        "lifecycle",
+        "fetch",
+        "create",
+        "update",
+        "drop",
+    ]
+
+    print(tabulate(critical + [SEPARATING_LINE] + all_others, headers=headers))
 
 
 if __name__ == "__main__":
