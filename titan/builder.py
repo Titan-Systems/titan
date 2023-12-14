ed7af9ee4bb86996fbe19217932fcef555d424a9
TJ Murphy <1796+teej@users.noreply.github.com>
2023-12-13 22:41:21 -0800

    Falsey bug in tidy_sql

--- titan/builder.py
+++ titan/builder.py
@@ -10,4 +10,4 @@ class SQL:
 def tidy_sql(*parts):
     if isinstance(parts[0], list):
         parts = parts[0]
-    return " ".join([str(part) for part in parts if part])
+    return " ".join([str(part) for part in parts if part != "" and part is not None])
