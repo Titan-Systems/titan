8d77d6c33d788860604b8915792f298b0444b5f0
TJ Murphy <1796+teej@users.noreply.github.com>
2024-02-22 23:24:35 -0800

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="titan",
-    version="0.1.4",
+    version="0.1.5",
     description="Snowflake infrastructure as code",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
