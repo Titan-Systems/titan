ed386de48c567e966ce8a8667d33e04893b5294f
TJ Murphy <1796+teej@users.noreply.github.com>
2024-10-29 19:34:28 -0700

    tests

--- tests/integration/data_provider/test_fetch_resource_simple.py
+++ tests/integration/data_provider/test_fetch_resource_simple.py
@@ -347,6 +347,11 @@ def test_fetch(
     if account_edition not in resource_fixture.edition:
         pytest.skip(f"Skipping test for {resource_fixture.__class__.__name__} on {account_edition} edition")
 
+    if os.environ["TEST_SNOWFLAKE_ACCOUNT"].endswith(".gcp"):
+        account_cloud = "gcp"
+    else:
+        account_cloud = "aws"
+
     if account_cloud == "gcp" and resource_fixture.__class__ == res.SnowflakeIcebergTable:
         pytest.skip("Skipping test for SnowflakeIcebergTable on GCP")
 
