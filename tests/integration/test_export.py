2e9830ec9a654ab8f89e0862e4dd9d1053b8f352
TJ Murphy <1796+teej@users.noreply.github.com>
2024-12-13 13:23:02 -0800

    export database name for schemas

--- tests/integration/test_export.py
+++ tests/integration/test_export.py
@@ -1,9 +1,22 @@
 import pytest
 
-from titan.operations.export import export_resources
+from titan.identifiers import URN, parse_FQN
+from titan.operations.export import export_resources, _format_resource_config
+from titan.enums import ResourceType
+from titan.data_provider import fetch_resource
 
 pytestmark = pytest.mark.requires_snowflake
 
 
 def test_export_all(cursor):
     assert export_resources(session=cursor.connection)
+
+
+def test_export_schema(cursor):
+    urn = URN(ResourceType.SCHEMA, parse_FQN("STATIC_DATABASE.STATIC_SCHEMA", is_db_scoped=True))
+    resource = fetch_resource(cursor, urn)
+    assert resource
+    resource_cfg = _format_resource_config(urn, resource, ResourceType.SCHEMA)
+    assert resource_cfg
+    assert "database" in resource_cfg
+    assert resource_cfg["database"] == "STATIC_DATABASE"
